9ba774c1b49ccd41dc8b91b5ee192d24eea490d7
tomoya ikeda <tikeda@tikeda.local>
2018-11-27 20:45:53 +0900

    2018/11/26

---
+++ ABC108/B.cpp
@@ -0,0 +1,60 @@
+//$g++ -std=c++11 Template.cpp 
+
+//#include <bits/stdc++.h>
+#include <iostream>
+#include <complex>
+#include <sstream>
+#include <string>
+#include <algorithm>
+#include <deque>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <vector>
+#include <set>
+#include <limits>
+#include <cstdio>
+#include <cctype>
+#include <cmath>
+#include <cstring>
+#include <cstdlib>
+#include <ctime>
+#include <climits>
+#define REP(i, n) for(int i = 0; i < (int)(n); i++)
+#define FOR(i, j, k) for(int i = (int)(j); i < (int)(k); ++i)
+#define ROF(i, j, k) for(int i = (int)(j); i >= (int)(k); --i)
+#define FORLL(i, n, m) for(long long i = n; i < (long long)(m); i++)
+#define SORT(v, n) sort(v, v+n)
+#define REVERSE(v) reverse((v).begin(), (v).end())
+
+using namespace std;
+using ll = long long;
+const ll MOD=1000000007LL;
+typedef pair<int, int> P;
+
+ll ADD(ll x, ll y) { return (x+y) % MOD; }
+ll SUB(ll x, ll y) { return (x-y+MOD) % MOD; }
+ll MUL(ll x, ll y) { return x*y % MOD; }
+ll POW(ll x, ll e) { ll v=1; for(; e; x=MUL(x,x), e>>=1) if (e&1) v = MUL(v,x); return v; }
+ll DIV(ll x, ll y) { /*assert(y%MOD!=0);*/ return MUL(x, POW(y, MOD-2)); }
+
+priority_queue<int> q_descending;
+priority_queue<int, vector<int>, greater<int> > q_ascending;
+
+int
+main(void){  
+  ios_base::sync_with_stdio(false);
+
+  int x1, y1, x2, y2;
+  cin >> x1 >> y1 >> x2 >> y2;
+  //int diff = (x2-x1)*(x2-x1) + (y2-y1)*(y2-y1);
+  //cout << x2-diff << " " << y2 << " " << x1-diff << " " << y1 << endl;
+
+  int diff_x = x2-x1;
+  int diff_y = y2-y1;
+
+  cout << x2-diff_y << " " << y2+diff_x << " " << x1-diff_y << " " << y1+diff_x << endl;
+  
+  return 0;
+}
