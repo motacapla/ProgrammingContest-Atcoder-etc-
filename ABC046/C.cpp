30b455a1c011363df929105fc07376fd5ab13d4a
motacapla <t.i.motacapla@gmail.com>
2019-01-23 13:15:59 +0900

    解説AC, TLEした

---
+++ ABC046/C.cpp
@@ -0,0 +1,62 @@
+//#include <bits/stdc++.h>
+#include <iostream>
+#include <complex>
+#include <sstream>
+#include <string>
+#include <algorithm>
+#include <deque>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <vector>
+#include <set>
+#include <limits>
+#include <cstdio>
+#include <cctype>
+#include <cmath>
+#include <cstring>
+#include <cstdlib>
+#include <ctime>
+#include <climits>
+#define REP(i, n) for(int i = 0; i < (int)(n); i++)
+#define FOR(i, j, k) for(int i = (int)(j); i < (int)(k); ++i)
+#define ROF(i, j, k) for(int i = (int)(j); i >= (int)(k); --i)
+#define FORLL(i, n, m) for(long long i = n; i < (long long)(m); i++)
+#define SORT(v, n) sort(v, v+n)
+#define REVERSE(v) reverse((v).begin(), (v).end())
+
+using namespace std;
+using ll = long long;
+const ll MOD=1000000007LL;
+typedef pair<int, int> P;
+
+ll ADD(ll x, ll y) { return (x+y) % MOD; }
+ll SUB(ll x, ll y) { return (x-y+MOD) % MOD; }
+ll MUL(ll x, ll y) { return x*y % MOD; }
+ll POW(ll x, ll e) { ll v=1; for(; e; x=MUL(x,x), e>>=1) if (e&1) v = MUL(v,x); return v; }
+ll DIV(ll x, ll y) { /*assert(y%MOD!=0);*/ return MUL(x, POW(y, MOD-2)); }
+
+ll nl, nm;
+
+int
+main(void){  
+  cin.tie(0);
+  ios::sync_with_stdio(false);
+
+  int n;
+  ll tkhs=1LL, aoki=1LL;
+  cin >> n;  
+  REP(i, n){
+    int t, a;
+    cin >> t >> a;
+    ll val = max(((tkhs+t-1)/t), ((aoki+a-1)/a));
+    aoki = val * a;
+    tkhs = val * t;
+  }
+  
+  cout << aoki + tkhs << endl;
+  
+  
+  return 0;
+}
