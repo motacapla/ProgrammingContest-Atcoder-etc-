388775cdce03db4f37c473c61ec2a1c623ebf86e
tomoya ikeda <tikeda@tikeda.local>
2019-01-31 21:08:30 +0900

    UFすらいらない

---
+++ ABC068/C.cpp
@@ -0,0 +1,76 @@
+//$g++ -std=c++11 Template.cpp 
+
+//#include <bits/stdc++.h>
+#include <iostream>
+#include <complex>
+#include <sstream>
+#include <string>
+#include <algorithm>
+#include <deque>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <vector>
+#include <set>
+#include <limits>
+#include <cstdio>
+#include <cctype>
+#include <cmath>
+#include <cstring>
+#include <cstdlib>
+#include <ctime>
+#include <climits>
+#include <iomanip>
+
+#define REP(i, n) for(int i = 0; i < (int)(n); i++)
+#define FOR(i, j, k) for(int i = (int)(j); i < (int)(k); ++i)
+#define ROF(i, j, k) for(int i = (int)(j); i >= (int)(k); --i)
+#define FORLL(i, n, m) for(long long i = n; i < (long long)(m); i++)
+#define SORT(v, n) sort(v, v+n)
+#define REVERSE(v) reverse((v).begin(), (v).end())
+
+using namespace std;
+using ll = long long;
+const ll MOD=1000000007LL;
+typedef pair<int, int> P;
+
+ll ADD(ll x, ll y) { return (x+y) % MOD; }
+ll SUB(ll x, ll y) { return (x-y+MOD) % MOD; }
+ll MUL(ll x, ll y) { return x*y % MOD; }
+ll POW(ll x, ll e) { ll v=1; for(; e; x=MUL(x,x), e>>=1) if (e&1) v = MUL(v,x); return v; }
+ll DIV(ll x, ll y) { /*assert(y%MOD!=0);*/ return MUL(x, POW(y, MOD-2)); }
+
+priority_queue<int> q_descending;
+priority_queue<int, vector<int>, greater<int> > q_ascending;
+
+P ab[2000001];
+
+int
+main(void){  
+  ios_base::sync_with_stdio(false);
+
+  int n, m;
+  cin >> n >> m;
+  
+  REP(i, m){
+    int a, b;
+    cin >> a >> b;
+    ab[i].first = a;
+    ab[i].second = b;
+  }
+  bool f = false;
+  sort(ab, ab+m);
+
+  set<int> s;
+  
+  REP(i, m) if(ab[i].first == 1) s.insert(ab[i].second);
+  REP(i, m) if(ab[i].second == n){
+    auto src = s.find(ab[i].first);
+    if(src != s.end()) f = true;
+  }
+  
+  cout << (f ? "POSSIBLE" : "IMPOSSIBLE") << endl;
+  
+  return 0;
+}
