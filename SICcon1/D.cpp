7d6eb21092b408e7ec866b6fbead481480dcb64a
tomoya ikeda <tikeda@tikeda.local>
2019-02-01 00:04:23 +0900

    未完成 TLE

--- SICcon1/D.cpp
+++ SICcon1/D.cpp
@@ -38,10 +38,12 @@ ll POW(ll x, ll e) { ll v=1; for(; e; x=MUL(x,x), e>>=1) if (e&1) v = MUL(v,x); 
 ll DIV(ll x, ll y) { /*assert(y%MOD!=0);*/ return MUL(x, POW(y, MOD-2)); }
 
 ll nl, nm;
-ll a[575777];
+ll a[10000001];
 
 map<ll,ll> mp;
-vector<set<ll>> vs;
+vector<set<ll>> vs(100001);
+
+// vs -> multisetで書き直しするか
 
 int
 main(void){  
@@ -53,41 +55,46 @@ main(void){
   REP(i, m) cin >> a[i];
 
   ll r = 0LL, minlen = 1e8+1;
-  ll sum = 0LL;
+  ll sum = 0LL, vitr = 0LL;;
   REP(l, m){    
     while(r < m && sum < n){
       sum += a[r];
       r++;
     }
-    //check
-    //cout << "l: " << l << " r: " << r << " sum: " << sum << endl;
-    //if(sum >= n){
-    //}
-    if(minlen > r-l+1){
-      minlen = r-l+1;
-      vs.clear();
-      FOR(i, l, r+1){
-	mp[i]++;
+    if(minlen >= r-l && sum >= n){
+      //check
+      //cout << "l: " << l << " r: " << r << " sum: " << sum << " minlen: " << minlen << endl;      
+      minlen = r-l;
+      FOR(i, l, r){
+	vs[vitr].insert(i);
       }
-    }    
+      vitr++;
+    }
     if(l == r) r++;
     else sum -= a[l];
   }
 
-  for(auto itr=vs.begin(); itr!=vs.end(); itr++){
-    (*itr)
+  multiset<int> aa, tmp, res;
+  int f = 0;
+  bool flag = false;
+
+  REP(i, vitr){
+    if(minlen != vs[i].size()) continue;//vs[i].clear(); //いらないやつは消す
+    else {
+      tmp.clear();
+      res.clear();
+      for(auto itr=vs[i].begin(); itr!=vs[i].end(); itr++) {
+	tmp.insert(*itr);       
+      }
+      f++;
+      flag = true;
+    }
+    if(f == 1) aa = tmp;
+    if(flag) set_intersection(aa.begin(), aa.end(), tmp.begin(), tmp.end(), inserter(res, res.end()));
+    flag = false;
   }
-  
-  //積集合の中で重なるもの
-  /*
-  bool f = false;
-  for(auto itr=mp.begin(); itr!=mp.end(); itr++){
-    cout << itr->second << endl;
-    f = true;
-    break;
-  }
-  if(!f) cout << "0" << endl;
-  */
+  if(f == 1) res = aa;
+  cout << res.size() << endl;
   
   return 0;
 }
