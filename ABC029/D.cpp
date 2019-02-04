cc33dcc5f030b612e13bc31b74c085740e240649
tomoya ikeda <tikeda@tikeda.local>
2019-02-04 21:56:13 +0900

    かいせつAC

--- ABC029/D.cpp
+++ ABC029/D.cpp
@@ -1,3 +1,5 @@
+//$g++ -std=c++11 Template.cpp 
+
 //#include <bits/stdc++.h>
 #include <iostream>
 #include <complex>
@@ -19,6 +21,8 @@
 #include <cstdlib>
 #include <ctime>
 #include <climits>
+#include <iomanip>
+
 #define REP(i, n) for(int i = 0; i < (int)(n); i++)
 #define FOR(i, j, k) for(int i = (int)(j); i < (int)(k); ++i)
 #define ROF(i, j, k) for(int i = (int)(j); i >= (int)(k); --i)
@@ -37,45 +41,34 @@ ll MUL(ll x, ll y) { return x*y % MOD; }
 ll POW(ll x, ll e) { ll v=1; for(; e; x=MUL(x,x), e>>=1) if (e&1) v = MUL(v,x); return v; }
 ll DIV(ll x, ll y) { /*assert(y%MOD!=0);*/ return MUL(x, POW(y, MOD-2)); }
 
-ll nl, nm;
+priority_queue<int> q_descending;
+priority_queue<int, vector<int>, greater<int> > q_ascending;
 
-vector<int> a;
-int cnt[200001];
+const int MAX = 1e9+1;
+
+int dp[11][2][11];
+ll n;
 
 int
-main(void){  
-  cin.tie(0);
-  ios::sync_with_stdio(false);
-
-  int n, k;
-  cin >> n >> k;
-  REP(i, n) {
-    int tmp;
-    cin >> tmp;
-    a.push_back(tmp);
-    cnt[tmp]++;
-  }
-
-  //REP(i, n+1) cout << cnt[i] << endl;  
-  
-  int kinds = 0;
-  FOR(i, 1, n+1) if(cnt[i]) kinds++;
-  if(kinds <= k){cout << 0 << endl; return 0;}
-
-  sort(cnt, cnt+n+1);
-
-  //REP(i, n+1) cout << cnt[i] << endl;
-  
-  ll ans = 0LL;
-  FOR(i, 1, n+1){
-    if(cnt[i]){
-      ans += cnt[i];
-      kinds--;
-      if(kinds <= k) break;
+main(void){
+  cin.tie(0);  
+  ios_base::sync_with_stdio(false);
+  cin >> n;
+  string s = to_string(n);  
+  int sz = s.size();
+  dp[0][0][0] = 1;
+  REP(i, sz) {
+    ll d = s[i]-'0';
+    REP(j, 2) {
+      ll lim=(j?9:d);    
+      REP(k, 10){
+	//k + 
+	REP(l, lim+1) dp[i+1][j||l!=lim][k+(l==1)] += dp[i][j][k];
+      }
     }
   }
-
+  ll ans = 0LL;
+  REP(i, 11) REP(j, 2) ans += dp[sz][j][i]*i;  
   cout << ans << endl;
-  
   return 0;
 }
