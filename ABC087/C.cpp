654cf9b1850e41ad0c8eb37afd96aac46947687a
motacapla <t.i.motacapla@gmail.com>
2019-02-06 12:54:55 +0900

    AC

---
+++ ABC087/C.cpp
@@ -0,0 +1,63 @@
+//#include <bits/stdc++.h>
+#include <iostream>
+#include <complex>
+#include <sstream>
+#include <string>
+#include <algorithm>
+#include <deque>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <vector>
+#include <set>
+#include <limits>
+#include <cstdio>
+#include <cctype>
+#include <cmath>
+#include <cstring>
+#include <cstdlib>
+#include <ctime>
+#include <climits>
+#define REP(i, n) for(int i = 0; i < (int)(n); i++)
+#define FOR(i, j, k) for(int i = (int)(j); i < (int)(k); ++i)
+#define ROF(i, j, k) for(int i = (int)(j); i >= (int)(k); --i)
+#define FORLL(i, n, m) for(long long i = n; i < (long long)(m); i++)
+#define SORT(v, n) sort(v, v+n)
+#define REVERSE(v) reverse((v).begin(), (v).end())
+
+using namespace std;
+using ll = long long;
+const ll MOD=1000000007LL;
+typedef pair<int, int> P;
+
+ll ADD(ll x, ll y) { return (x+y) % MOD; }
+ll SUB(ll x, ll y) { return (x-y+MOD) % MOD; }
+ll MUL(ll x, ll y) { return x*y % MOD; }
+ll POW(ll x, ll e) { ll v=1; for(; e; x=MUL(x,x), e>>=1) if (e&1) v = MUL(v,x); return v; }
+ll DIV(ll x, ll y) { /*assert(y%MOD!=0);*/ return MUL(x, POW(y, MOD-2)); }
+
+int a[3][110];
+
+ll nl, nm;
+
+int
+main(void){  
+  cin.tie(0);
+  ios::sync_with_stdio(false);
+
+  int n;
+  cin >> n;
+  REP(i, 2) REP(j, n) cin >> a[i][j];
+
+  REP(j, n-1) a[0][j+1] += a[0][j];
+  a[1][0] += a[0][0];
+
+  FOR(j, 1, n){
+    a[1][j] += max(a[0][j], a[1][j-1]);
+  }
+
+  cout << a[1][n-1] << endl;
+  
+  return 0;
+}
