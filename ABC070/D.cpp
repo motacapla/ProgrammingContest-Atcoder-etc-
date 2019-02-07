88541f6d309b1eccb52b1c3102e3e7a31cdbab3e
tomoya ikeda <tikeda@tikeda.local>
2019-02-08 07:33:31 +0900

    解説AC 中間値をベースとした最短経路

--- ABC070/D.cpp
+++ ABC070/D.cpp
@@ -1,68 +1,107 @@
+//$g++ -std=c++11 Template.cpp 
+
+//#include <bits/stdc++.h>
 #include <iostream>
-#include <vector>
+#include <complex>
+#include <sstream>
+#include <string>
+#include <algorithm>
+#include <deque>
+#include <list>
+#include <map>
+#include <numeric>
 #include <queue>
+#include <vector>
+#include <set>
+#include <limits>
+#include <cstdio>
+#include <cctype>
+#include <cmath>
+#include <cstring>
+#include <cstdlib>
+#include <ctime>
+#include <climits>
+#include <iomanip>
+
+#define REP(i, n) for(int i = 0; i < (int)(n); i++)
+#define FOR(i, j, k) for(int i = (int)(j); i < (int)(k); ++i)
+#define ROF(i, j, k) for(int i = (int)(j); i >= (int)(k); --i)
+#define FORLL(i, n, m) for(long long i = n; i < (long long)(m); i++)
+#define SORT(v, n) sort(v, v+n)
+#define REVERSE(v) reverse((v).begin(), (v).end())
+
 using namespace std;
-
 using ll = long long;
-const int limit = 100010;
-using edge = struct {int to; ll cost;};
-vector<edge> tree[limit];
-ll depth[limit];
+const ll MOD=1000000007LL;
+typedef pair<int, int> P;
 
-void BFS(int v){
+ll ADD(ll x, ll y) { return (x+y) % MOD; }
+ll SUB(ll x, ll y) { return (x-y+MOD) % MOD; }
+ll MUL(ll x, ll y) { return x*y % MOD; }
+ll POW(ll x, ll e) { ll v=1; for(; e; x=MUL(x,x), e>>=1) if (e&1) v = MUL(v,x); return v; }
+ll DIV(ll x, ll y) { /*assert(y%MOD!=0);*/ return MUL(x, POW(y, MOD-2)); }
+
+priority_queue<int> q_descending;
+priority_queue<int, vector<int>, greater<int> > q_ascending;
+
+#define MAX_N 10000001
+
+int n;
+
+struct edge {
+  int from, to;
+  ll cost;
+    edge(int to, ll cost): to(to), cost(cost) {}
+};
+
+vector<edge> graph[MAX_N];
+ll d[MAX_N];
+
+void bfs(int k){
   queue<int> q;
-  int i=0;
-  visited[v] = 1;
-  q.push(v);
-  while(q.size()!=0){
-    v = q.front();
-    q.pop();
-    int d=0;
-    while(tree[v].next != 0){
-      i = tree[v].next;
-      d += tree[v].val;
-      visited[i] = 1;
-      best_dst[i] = d;
-      q.push(i);
+  q.push(k);
+  while(!q.empty()){
+    int from = q.front(); q.pop();
+    REP(i, graph[from].size()){
+      edge e = graph[from][i];
+      int to = graph[from][i].to;
+      ll cost = graph[from][i].cost;
+      if(d[to]!=0) continue;
+      d[to] = d[from] + cost;
+      q.push(to);
     }
   }
 }
 
-int main(int argc, char* argv[]){  
-  int n;
-  cin >> n;
-
- for (int i = 0; i < n - 1; ++i) {
-   int a, b, c;
-   cin >> a >> b >> c;
-   a--, b--;
-   tree[a].push_back({b, c});
-   tree[b].push_back({a, c});
- }
-  /*
-  for(int i=0; i<N; i++){
-    visited[i] = 0;
-  }
-
-  for(int i=0; i<N; i++){
-    cout << i << " : " << tree[i].next << endl;
-  }
-  cin >> Q >> K;
-
-  BFS(K-1);
+int
+main(void){  
+  ios_base::sync_with_stdio(false);
   
-  for(int i=0; i<Q; i++){
-    cin >> x >> y;
-    //cout << best_dst[x-1] + best_dst[y-1] << endl;
+  cin >> n;
+  REP(i, n-1) {
+    ll a, b, c;
+    cin >> a >> b >> c;
+    a--; b--;
+    edge e1 = {(int)b, c};
+    edge e2 = {(int)a, c};
+    graph[a].emplace_back(e1);
+    graph[b].emplace_back(e2);
   }
 
-  for(int i=0; i<N; i++){
-    cout << i << ":" << best_dst[i] << endl;
+  //wf まにあわない
+  int q, k;
+  cin >> q >> k;
+  k--;
+
+  //kから各頂点へのの最短距離を求める
+  bfs(k);
+  
+  REP(i, q){    
+    int x, y;
+    cin >> x >> y;
+    x--; y--;
+    cout << d[x] + d[y] << endl;
   }
-  */
+  
   return 0;
 }
-
-  
-
-
