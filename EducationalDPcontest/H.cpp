70dbeea03e6a92b3c1c156817d4e1022332c241e
tomoya ikeda <tikeda@tikeda.local>
2019-02-21 20:14:53 +0900

    AC

---
+++ EducationalDPcontest/H.cpp
@@ -0,0 +1,60 @@
+//#include <bits/stdc++.h>
+#include <iostream>
+#include <complex>
+#include <sstream>
+#include <string>
+#include <algorithm>
+#include <deque>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <vector>
+#include <set>
+#include <limits>
+#include <cstdio>
+#include <cctype>
+#include <cmath>
+#include <cstring>
+#include <cstdlib>
+#include <ctime>
+#include <climits>
+#include <iomanip>
+
+#define REP(i, n) for(int i = 0; i < (int)(n); i++)
+#define FOR(i, j, k) for(int i = (int)(j); i < (int)(k); ++i)
+#define ROF(i, j, k) for(int i = (int)(j); i >= (int)(k); --i)
+#define FORLL(i, n, m) for(long long i = n; i < (long long)(m); i++)
+#define SORT(v, n) sort(v, v+n)
+#define REVERSE(v) reverse((v).begin(), (v).end())
+
+using namespace std;
+using ll = long long;
+const ll MOD=1000000007LL;
+typedef pair<int, int> P;
+
+ll ADD(ll x, ll y) { return (x+y) % MOD; }
+ll SUB(ll x, ll y) { return (x-y+MOD) % MOD; }
+ll MUL(ll x, ll y) { return x*y % MOD; }
+ll POW(ll x, ll e) { ll v=1; for(; e; x=MUL(x,x), e>>=1) if (e&1) v = MUL(v,x); return v; }
+ll DIV(ll x, ll y) { /*assert(y%MOD!=0);*/ return MUL(x, POW(y, MOD-2)); }
+
+char a[1010][1010];
+ll dp[1010][1010];
+
+int
+main(void){  
+  int h, w;
+  cin >> h >> w;
+  REP(i, h) REP(j, w) cin >> a[i][j];
+
+  dp[0][0] = 1;
+  REP(i, h) REP(j, w){
+    if(i+1 <= h && a[i][j] == '.') dp[i+1][j] = ADD(dp[i+1][j], dp[i][j]);
+    if(j+1 <= w && a[i][j] == '.') dp[i][j+1] = ADD(dp[i][j+1], dp[i][j]);
+  }
+
+  cout << dp[h-1][w-1] << endl;
+  
+  return 0;
+}
