71e741ac731568f080856397389da5fbc5e4fc71
tomoya ikeda <tikeda@tikeda.local>
2019-02-24 20:04:24 +0900

    TLE, 解説AC a/A[i]で割り切れないとき+1する

---
+++ ARC/ARC062/C.cpp
@@ -0,0 +1,64 @@
+//#include <bits/stdc++.h>
+#include <iostream>
+#include <complex>
+#include <sstream>
+#include <string>
+#include <algorithm>
+#include <deque>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <vector>
+#include <set>
+#include <limits>
+#include <cstdio>
+#include <cctype>
+#include <cmath>
+#include <cstring>
+#include <cstdlib>
+#include <ctime>
+#include <climits>
+#include <iomanip>
+
+#define REP(i, n) for(int i = 0; i < (int)(n); i++)
+#define FOR(i, j, k) for(int i = (int)(j); i < (int)(k); ++i)
+#define ROF(i, j, k) for(int i = (int)(j); i >= (int)(k); --i)
+#define FORLL(i, n, m) for(long long i = n; i < (long long)(m); i++)
+#define SORT(v, n) sort(v, v+n)
+#define REVERSE(v) reverse((v).begin(), (v).end())
+
+using namespace std;
+using ll = long long;
+const ll MOD=1000000007LL;
+typedef pair<int, int> P;
+
+ll ADD(ll x, ll y) { return (x+y) % MOD; }
+ll SUB(ll x, ll y) { return (x-y+MOD) % MOD; }
+ll MUL(ll x, ll y) { return x*y % MOD; }
+ll POW(ll x, ll e) { ll v=1; for(; e; x=MUL(x,x), e>>=1) if (e&1) v = MUL(v,x); return v; }
+ll DIV(ll x, ll y) { /*assert(y%MOD!=0);*/ return MUL(x, POW(y, MOD-2)); }
+
+template<class T> bool chmax(T &a,const T &b){if(a<b){a=b;return 1;}return 0;}
+template<class T> bool chmin(T &a,const T &b){if(a>b){a=b;return 1;}return 0;}
+
+int n;
+int T[1010], A[1010];
+
+int
+main(void){  
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+
+  cin >> n;
+  REP(i, n) cin >> T[i] >> A[i];
+
+  ll t=1, a=1;
+  REP(i, n){
+    ll v = max(a/A[i] + (a%A[i]? 1 : 0), t/T[i] + (t%T[i]? 1 : 0));    
+    t = v * T[i];
+    a = v * A[i];    
+  }  
+  cout << t+a << endl;
+  return 0;
+}
