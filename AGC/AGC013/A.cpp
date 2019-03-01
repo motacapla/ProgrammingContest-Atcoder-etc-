b7cc15d0a6298698bddc364f844d8f2fbe749688
tomoya ikeda <tikeda@tikeda.local>
2019-03-01 23:07:53 +0900

    AC 30分ぐらいかけた... 脳バグってる

---
+++ AGC/AGC013/A.cpp
@@ -0,0 +1,117 @@
+//#include <bits/stdc++.h>
+#include <iostream>
+#include <complex>
+#include <sstream>
+#include <string>
+#include <algorithm>
+#include <deque>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <vector>
+#include <set>
+#include <limits>
+#include <cstdio>
+#include <cctype>
+#include <cmath>
+#include <cstring>
+#include <cstdlib>
+#include <ctime>
+#include <climits>
+#include <iomanip>
+
+#define REP(i, n) for(int i = 0; i < (int)(n); i++)
+#define FOR(i, j, k) for(int i = (int)(j); i < (int)(k); ++i)
+#define ROF(i, j, k) for(int i = (int)(j); i >= (int)(k); --i)
+#define FORLL(i, n, m) for(long long i = n; i < (long long)(m); i++)
+#define SORT(v, n) sort(v, v+n)
+#define REVERSE(v) reverse((v).begin(), (v).end())
+
+using namespace std;
+using ll = long long;
+const ll MOD=1000000007LL;
+typedef pair<int, int> P;
+
+ll ADD(ll x, ll y) { return (x+y) % MOD; }
+ll SUB(ll x, ll y) { return (x-y+MOD) % MOD; }
+ll MUL(ll x, ll y) { return x*y % MOD; }
+ll POW(ll x, ll e) { ll v=1; for(; e; x=MUL(x,x), e>>=1) if (e&1) v = MUL(v,x); return v; }
+ll DIV(ll x, ll y) { /*assert(y%MOD!=0);*/ return MUL(x, POW(y, MOD-2)); }
+
+template<class T> bool chmax(T &a,const T &b){if(a<b){a=b;return 1;}return 0;}
+template<class T> bool chmin(T &a,const T &b){if(a>b){a=b;return 1;}return 0;}
+
+int
+main(void){  
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+
+  int n;
+  cin >> n;
+  vector<int> A(n);
+  vector<int> dif(n);
+  REP(i,n) {
+    cin >> A[i];
+    if(i>0) dif[i] = A[i]-A[i-1];
+  }
+  int ans = 0;
+  
+  int l = 0;
+  if(n == 1){cout << 1 << endl; return 0;}
+  
+  bool plus =((A[1] >= A[0]) ? true : false);
+  bool start = false;
+  
+  FOR(i,1,n-1){
+    if(A[i+1] == A[i]) continue;
+    if(start){
+      plus = (A[i+1] > A[i] ? true : false);
+      start = false;
+    }
+    else{
+      if(A[i+1] > A[i]){
+	if(!plus){
+	  ans++;
+	  //continue;
+	  start = true;
+	}
+      }
+      if(A[i+1] < A[i]){
+	if(plus){
+	  ans++;
+	  start = true;
+	}
+      }    
+    }
+    //cout << "i: " << i << " start: " << start << " plus:" << plus << endl;
+  }
+  
+  cout << ans+1 << endl;
+  return 0;
+}
+
+
+  /*
+  FOR(i,1,n-1){
+    if(A[i+1] == A[i]) continue;
+    if(A[i+1] > A[i]){
+      if(start){
+	plus = true;
+      }
+      if(!plus){
+	ans++;
+	start = true;
+      }
+    }
+    if(A[i+1] < A[i]){
+      if(start){
+	plus = false;
+      }
+      if(plus){
+	ans++;
+	start = true;
+      }
+    }
+  }
+  */
